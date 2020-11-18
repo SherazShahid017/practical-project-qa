bacce83e81820ee82f64f0f19dc77f3a0da8174c
Ubuntu <ubuntu@ip-172-31-42-94.eu-west-1.compute.internal>
2020-11-18 18:09:40 +0000

    terraform added

---
+++ terraform-stuff/README.md
@@ -0,0 +1,17 @@
+README for Terraform tasks.
+
+Code will deploy an EC2 instance in eu-west-1, connecting to the subnet with a route table, within a public subnet, within a VPC, connecting via an Internet Gateway.
+
+Infrastructure:
+
+<ul>
+<li>VPC</li>
+<li>Public Subnet</li>
+<li>Route Table</li>
+<li>Internet Gateway</li>
+<li>Security Group(s)</li>
+<li>Private Subnet</li>
+<li>RDS Instance in private subnet</li>
+</ul>
+
+![Imgur](https://imgur.com/ZorO1Kq.png)
---
+++ terraform-stuff/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Installing pip and ansible
+sudo apt-get update
+sudo apt install python3-pip -y
+sudo apt install ansible -y
+
+# install ansible
+#git clone https://github.com/LukeBenson/install-scripts.git
+#cd install-scripts/
+#./ansible-install.sh
+#cd ~
+#rm -rf install-scripts/
+
+#install my ansible files
+git clone https://github.com/SherazShahid017/ansible-files.git
+cd ansible-files/
+
+#set the local-ip variable to the ec2-ip output
+#export localip=${terraform output ec2-ip}
+ansible-playbook -i inventory.conf playbook.yaml
