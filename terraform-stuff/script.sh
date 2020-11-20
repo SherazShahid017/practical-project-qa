d10ceb67131bfb4979ad05a2aca1f18fe1b78fb9
Ubuntu <ubuntu@ip-172-31-42-94.eu-west-1.compute.internal>
2020-11-20 09:51:27 +0000

    CHanges to bash script

--- terraform-stuff/script.sh
+++ terraform-stuff/script.sh
@@ -6,21 +6,13 @@ sudo apt install python3-pip -y
 sudo apt install python-pip -y
 sudo apt install ansible -y
 
-# install ansible
-#git clone https://github.com/LukeBenson/install-scripts.git
-#cd install-scripts/
-#./ansible-install.sh
-#cd ~
-#rm -rf install-scripts/
 
 #install my ansible files
 git clone https://github.com/SherazShahid017/practical-project-qa/
 cd practical-project-qa/
-git branch kubernetes
+git checkout kubernetes
 cd ansible-files/
 
-#set the local-ip variable to the ec2-ip output
-#export localip=${terraform output ec2-ip}
 ansible-playbook -i inventory.conf playbook.yaml
 
 
