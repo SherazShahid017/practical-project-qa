e2818f3a334b86b40c0e3918fad967553a5d8d7c
SherazShahid017 <SShahid2@qa.com>
2020-11-21 12:00:52 +0000

    script file indentation

--- script.sh
+++ script.sh
@@ -2,22 +2,22 @@
 
 ssh 52.214.30.196 << EOF
 
-git clone https://github.com/LukeBenson/install-scripts.git
-cd install-scripts/
-./docker_install.sh
-./docker-compose-install.sh
-cd ~
-rm -rf install-scripts/
-
-
-git clone https://github.com/SherazShahid017/practical-project-qa
-cd practical-project-qa/
-git checkout pytest
-docker-compose up -d
-
-docker exec backend bash -c "pytest tests/ --cov application"
-docker exec frontend bash -c "pytest tests/ --cov application"
-
-docker-compose down -d
-
+	git clone https://github.com/LukeBenson/install-scripts.git
+	cd install-scripts/
+	./docker_install.sh
+	./docker-compose-install.sh
+	cd ~
+	rm -rf install-scripts/
+	
+	
+	git clone https://github.com/SherazShahid017/practical-project-qa
+	cd practical-project-qa/
+	git checkout pytest
+	docker-compose up -d
+		
+	docker exec backend bash -c "pytest tests/ --cov application"
+	docker exec frontend bash -c "pytest tests/ --cov application"
+	
+	docker-compose down -d
+	
 EOF
