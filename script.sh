892d350d4761d7cea91b978abdb9e346c450e7ed
SherazShahid017 <SShahid2@qa.com>
2020-11-22 21:01:34 +0000

    Changes to script.sh

--- script.sh
+++ script.sh
@@ -29,6 +29,7 @@ ssh -i ~/.ssh/id_rsa ubuntu@18.202.167.4 << EOF
 
 	mysql -h terraform-20201122151632162900000001.cr9zze1q6zyi.eu-west-1.rds.amazonaws.com -P 3306 -u admin -proot1234 < ~/practical-project-qa/database/Create.sql;
 	
+	mysql -h terraform-20201122151632166500000002.cr9zze1q6zyi.eu-west-1.rds.amazonaws.com -P 3306 -u admin -proot1234 < ~/practical-project-qa/database/Create.sql	
 	########################### Running tests ################################
 	sudo docker-compose up -d
 		
